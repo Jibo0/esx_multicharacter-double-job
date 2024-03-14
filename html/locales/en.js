5b66cca4b2528608145b719b18f6534606d53e47
Jibo <81716405+Jibo0@users.noreply.github.com>
2024-03-14 23:01:19 +0100

    Add files via upload

---
+++ html/locales/en.js
@@ -0,0 +1,9 @@
+const translate = new Object();
+
+translate.name = "Nome";
+translate.job = "Lavoro";
+translate.job2 = "Lavoro 2";
+translate.bank = "Banca";
+translate.money = "Contanti";
+translate.gender = "Sesso";
+translate.dob = "Data di nascita";
